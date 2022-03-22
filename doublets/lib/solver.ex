8ac9f277f4c65c8d63a3031fb5fe3ece793f0f24
oswaldo falcon reyes <oswaldofalconreyes@MacBook-Air-de-oswaldo.local>
2022-03-22 10:43:55 -0600

    DONE ex

--- doublets/lib/solver.ex
+++ doublets/lib/solver.ex
@@ -1,26 +1,60 @@
 defmodule Doublets.Solver do
-  @dictionary "./resources/words.txt" |> File.read!() |> String.split()
 
-  def seleccion(word) do
-    Enum.filter(@dictionary, fn x -> String.length(x) == String.length(word) and word != x end)
+  @words "./resources/words.txt"
+    |> File.read!
+    |> String.split("\n")
+
+  def doublets(word_1, word_2) do
+    doublets_impl([[word_1]], word_2)
   end
 
-  def variantes(palabra) do
-    varia = seleccion(palabra)
+  def doublets_impl([], _word_2), do: []
 
-    freq_varia = varia |> Enum.map(fn x ->
-        List.zip([x |> String.graphemes(), palabra 
-        |> String.graphemes()])
-        |> Enum.map(fn {k, v} -> k == v end)
-        |> Enum.count(fn x -> x == false end)
-      end)
+  def doublets_impl(word_seqs, word_2) do
+    IO.inspect(word_seqs, label: "input")
 
-    List.zip([freq_varia, varia])
-    |> Enum.filter(fn {k, v} -> k == 1 end)
-    |> Enum.map(fn {_k, v} -> v end)
+    seqs_variants =
+      Enum.flat_map(word_seqs, fn seq -> complete_seq_variants(seq) end)
+      |> IO.inspect(label: "variants")
+
+    case find_solution(seqs_variants, word_2) do
+      nil -> doublets_impl(seqs_variants, word_2)
+      sol -> sol
+    end
   end
 
-  def doublets(word1, word2) do
-    "make me work"
+  def find_solution(word_seqs, target) do
+    Enum.find(word_seqs, fn seq -> last_word(seq) == target end)
   end
+
+  def complete_seq_variants(word_seq) do
+    variants =
+      word_seq
+      |> last_word()
+      |> find_variants()
+      |> Enum.filter(fn w -> not(w in word_seq) end)
+    for v <- variants, do: word_seq ++ [v]
+  end
+
+  def find_variants(word) do
+    word
+    |> same_length_words()
+    |> Enum.filter(fn w -> distance(word, w) == 1 end)
+  end
+
+  def same_length_words(word) do
+    @words
+    |> Enum.filter(fn w -> String.length(w) == String.length(word) end)
+    |> List.delete(word)
+  end
+
+  def distance(word_1, word_2) do
+    letters_1 = String.graphemes(word_1)
+    letters_2 = String.graphemes(word_2)
+
+    Enum.zip_with(letters_1, letters_2, fn a, b -> a == b end)
+    |> Enum.count(&(not &1))
+  end
+
+  def last_word(word_seq), do: Enum.at(word_seq, -1)
 end
