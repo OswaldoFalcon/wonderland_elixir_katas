fd3e816fe43958573ec96c3c0f27b1d7bbd8ac73
OswaldoFalcon <owa11@hotmail.com>
2022-03-30 09:55:33 -0600

    Avance 1

--- fox_goose_bag_of_corn/lib/puzzle.ex
+++ fox_goose_bag_of_corn/lib/puzzle.ex
@@ -1,36 +1,63 @@
-defmodule FoxGooseBagOfCorn.Puzzle do
+defmodule FoxGooseBagOfCorn do
 
-  @start_position [
-    [[:fox, :goose, :corn, :you], [:boat], []]
-  ]
+  @listaRestricciones [[:fox, :goose],[:goose, :corn]]
 
-  def river_crossing_plan do
-     # Implement this with an algorithm.
-     # Hard coding the solution is easy! ;-)
-    [
-      [[:fox, :goose], [:boat, :you, :corn, :goose], [:you]],
-      [[:goose, :corn], [:boat], [:you]]
-    ] 
+
+  #def hello do
+   #lista = [[:fox, :goose, :corn, :you], [:boat],[]]
+    #        [[:fox,:corn] [:boat ,:goose, :you] []]
+     #       [[:fox,:corn] [:boat] [,:goose, :you]]
+      #      [[:fox,:corn] [:boat, :you] [,:goose]]
+       #     [[:fox,:corn, :you] [:boat] [,:goose]]
+        #    [[:corn] [:boat, :you,:fox] [,:goose]]
+         #   [[:corn] [:boat] [,:goose,:fox, :you]]
+        #    [[:corn] [:boat, :you, :goose] [:fox]]
+         #  [[:corn, :you, :goose] [:boat] [:fox]]
+          #  [[:goose] [:boat,:corn, :you] [:fox]]
+           # [[:goose] [:boat] [:fox,:corn, :you]]
+            #[[:goose] [:boat, :you] [:fox,:corn]]
+           # [[:goose, :you] [:boat] [:fox,:corn]]
+           # [[] [:boat,:goose, :you] [:fox,:corn]]
+           # [[] [:boat] [:fox,:corn, :goose, :you]]
+
+  #end 
+def solver() do
+    listaActual= [[:corn], [:boat], [:goose,:fox, :you]]
+
+    #nos dice donde esta el :you
+    posi= Enum.find_index(listaActual,fn x -> Enum.member?(x, :you)end)
+
+    case posi do
+      #regresa los posibles elementos a mover
+      0 -> b = Enum.at(listaActual,0) |> List.delete(:you)
+      #concatenar el elemento a lista del bote
+      |> Enum.map(fn x ->
+        #mueve el :you y un elemento al :bote
+        List.update_at(listaActual,1,&(&1++[x,:you]))
+        #elimina el elemento que se movio de la lista de la orilla
+        |>List.update_at(0,&List.delete(&1,x))
+        |>List.update_at(0,&List.delete(&1,:you))
+
+
+      end)
+
+     1 -> a=Enum.at(listaActual,1) |> List.delete(:boat)
+     listaActual = List.update_at(listaActual,1,&(&1=[:boat]))
+     [List.update_at(listaActual,0, &(&1++a)),List.update_at(listaActual,2, &(&1++a)) ]
+
+     2 ->  c = Enum.at(listaActual,2) |> List.delete(:you)
+     #concatenar el elemento a lista del bote
+     |> Enum.map(fn x ->
+       List.update_at(listaActual,1,&(&1++[x,:you]))
+       |>List.update_at(2,&List.delete(&1,x))
+       |>List.update_at(2,&List.delete(&1,:you))
+
+    end)
   end
+end
+  def verificacion(listaPrincipal) do
+    #Busca estados restringidos de los cuales retorna una lista de listas con booleanos
+    Enum.any?(listaPrincipal, fn x -> Enum.any?(@listaRestricciones, &(&1 == x)) end)
 
-  def to_set(list) when is_list(list), do: Enum.into(list, MapSet.new)
-  def step_to_sets(step), do: step |> Enum.map(&to_set/1)
-
-  @doc """
-    Defines a sigil for a HashSet of atoms
-
-    Example: 
-    iex> import #{__MODULE__}
-    iex> ~H":a :b :c"
-    #MapSet<[:a, :b, :c]>
-    iex> ~H""
-    #MapSet<[]>
-  """
-  def sigil_H(str, _opts) do
-    str |> String.split(" ") 
-        |> Enum.filter(&String.length(&1) > 0)
-        |> Enum.map(&String.replace(&1,":","")) 
-        |> Enum.map(&String.to_atom/1)
-        |> Enum.into(MapSet.new)
   end
 end
